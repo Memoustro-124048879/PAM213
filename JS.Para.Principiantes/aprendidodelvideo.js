ec8e2bddb008259ba2ad503910b071f8a8918b03
Memoustro-124048879 <124048879@upq.edu.mx>
2025-09-10 10:26:28 -0600

    Utilización de variables
    Creamos variables y las definimos, para después mandarlas a llamar junto con sus datos

--- JS.Para.Principiantes/aprendidodelvideo.js
+++ JS.Para.Principiantes/aprendidodelvideo.js
@@ -1,6 +1,4 @@
-console.log('this is a string');
-console.log('14.3');
-console.log(true);
-console.log([1, 2, 3]);
-console.log({"username": "ryan",
-    "score": 70.4});
+var nameuser = "john";
+let lastname = "carter";
+
+console.log(lastname)
