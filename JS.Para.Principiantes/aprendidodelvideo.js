6f595c6190dd20d1ab49cdd22c8c6fe07f41531d
Memoustro-124048879 <124048879@upq.edu.mx>
2025-09-10 10:19:07 -0600

    Aplicación de los datos
    Ahora aplicamos lo aprendido de los datos haciendo console.log y verificamos en la consola del navegador

--- JS.Para.Principiantes/aprendidodelvideo.js
+++ JS.Para.Principiantes/aprendidodelvideo.js
@@ -1,31 +1,6 @@
-// tipos de datos
-"Hello World" // string
-'Hello World' // string
-
-100000 // number
--2.3
-
-// boolean
-true
-false
-
-// array
-['joe', 'ryan', 'martha']
-[1, 2, 3]
-[true, false, true, false]
-
-//object
-
-{
-    "username": 'ryan',
-    "score": 70.4,
-    "hours": 14,
-    "proffesional2": true 
-}
-
-{
-    "username": 'joe',
-    "score": 0.4,
-    "hours": 1,
-    "proffesional2": false 
-}
+console.log('this is a string');
+console.log('14.3');
+console.log(true);
+console.log([1, 2, 3]);
+console.log({"username": "ryan",
+    "score": 70.4});
