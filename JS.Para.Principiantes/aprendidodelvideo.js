9b7332520bb7a81c40d96bdbf19afb956e05b76d
Memoustro-124048879 <124048879@upq.edu.mx>
2025-09-10 10:12:09 -0600

    Explicación de tipos de dato
    Añadimos diferentes tipos de datos y nos fueron explicando paso a paso que eran, como se dividían y como se conformaban

---
+++ JS.Para.Principiantes/aprendidodelvideo.js
@@ -0,0 +1,31 @@
+// tipos de datos
+"Hello World" // string
+'Hello World' // string
+
+100000 // number
+-2.3
+
+// boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+//object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional2": true 
+}
+
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional2": false 
+}
