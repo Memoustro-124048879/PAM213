9f39b7fc6a3f67b2d57e7162228bdc88ad374a80
Memoustro-124048879 <124048879@upq.edu.mx>
2025-09-25 15:13:55 -0600

    Utilización de Async & Await junto a una promesa
    La estructura del Async & Await llega a ser confusa al momento de implementarla, pero lo que al final me termino devolviendo fue la utilización de la función ""SimularPeticionAPI

---
+++ Practica3/Ej4.js
@@ -0,0 +1,21 @@
+function simularPeticionAPI(){
+    return new Promise(resolve=>{
+        setTimeout(() =>{
+            resolve('Datos recibidos correctamente');
+        }, 5000);
+    });
+}
+
+async function obtenerDatos() {
+    try{
+        const respuesta = await (simularPeticionAPI) ;
+
+        console.log('Proceso terminado');
+        console.log('Resultado de la petición:', respuesta);
+        
+    } catch(error){
+        console.log('Ocurrio un error:', error.message);
+    }
+}
+
+obtenerDatos();
