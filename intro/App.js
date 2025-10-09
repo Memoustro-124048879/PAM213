e70974735d6071da7bc0fdd60e6913a2667911c1
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-09 14:54:51 -0600

    Quitamos lo inservible  y dejamos lo importante

--- intro/App.js
+++ intro/App.js
@@ -1,32 +1,9 @@
-//1. imports: Zona de importaciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React,{useState}from 'react';
 
-//2. Main: Zona de componentes
+import ContadorScreen from './screens/contadorScreen';
+
 export default function App() {
 
-  const [contador,setContador]=useState(0); /* Destructuración */
-
   return (
-    <View style={styles.container}>
-      <Text> Contador: {contador} </Text> {/* soy un comentario */}
-     {/*  <Button></Button> */}
-      <Button title="Agregar" onPress={()=>setContador(contador+1)}/> {/* Otra forma de agregar un boton */}
-      <Button title="Quitar" onPress={()=>setContador(contador-1)}/> {/* Desminuimos al contador */}
-      <Button title="Reiniciar" onPress={()=>setContador(contador-contador)}/> {/* Reiniciamos el contador */}
-      <StatusBar style="auto" />
-
-    </View>
+    <ContadorScreen></ContadorScreen>
   );
 }
-
-//3. Estilos: Zona de estetica y posicionamiento
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
