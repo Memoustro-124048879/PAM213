a4b6eacf08b1b0dd4e19885e9c0fd626ab699512
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-15 19:28:37 -0600

    Declaración del estado e inicialización del menu

--- intro/screens/MenuScreen.js
+++ intro/screens/MenuScreen.js
@@ -1,9 +1,19 @@
 import { Text, StyleSheet, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import contadorScreen from './contadorScreen'
 import BotonesSceen from './BotonesSceen'
 
 export default function MenuScreen() {
+
+    const [screen, setScreen] = useState('menu');
+
+    switch(screen) {
+        case 'contador':
+            return <contadorScreen/>
+        case 'botones':
+            return <BotonesSceen/>
+    }
+
     return (
       <View>
         <Text>MenuScreen</Text>
