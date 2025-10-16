62c772b3a7c2b2c40f8b55ccd60e6bc8f680289e
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-15 19:39:01 -0600

    Creamos el esqueleto de botones

---
+++ intro/screens/BotonesScreen.js
@@ -0,0 +1,11 @@
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function BotonesScreen()  {
+    return (
+      <View>
+        <Text>Proximamente por Jose Maria & equipo</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
