431fe1e9b1e81fa8426b5e4c3f12d3d5a1667730
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-15 19:47:02 -0600

    No jalaba y tuve que cambiarle unas cositas

--- intro/screens/MenuScreen.js
+++ intro/screens/MenuScreen.js
@@ -1,26 +1,30 @@
 import { Text, StyleSheet, View, Button } from 'react-native'
-import React, { use, useState } from 'react'
-import contadorScreen from './contadorScreen'
-import BotonesSceen from './BotonesSceen'
+import React, { useState } from 'react'
+import ContadorScreen from './ContadorScreen'
+import BotonesScreen from './BotonesScreen'
 
 export default function MenuScreen() {
 
-    const [screen, setScreen] = useState('menu');
+    const [screen, setScreen] = useState('menu'); /* desestructuración */ /* declaración del estado e inicialización del menu */
 
-    switch(screen) {
-        case 'contador':
-            return <contadorScreen/>
-        case 'botones':
-            return <BotonesSceen/>
+    switch (screen) {
+      case 'contador':
+        return <ContadorScreen />;
+      case 'botones':
+        return <BotonesScreen />;
         case 'menu':
             default:
                 return (
                     <View>
-                        <Text>Menu de Practicas</Text>
-                        <Button title='Pract: Contador' onPress={()=>setScreen('contador')}/>
+                        <Text>Menú de Prácticas</Text>
+                        <Button title='Pract: Contador' onPress={()=>setScreen('contador')}/>{/* el valor que esté dentro de setScreen debe ser igual al del switch */}
+                        <Button title='Pract: Buttons' onPress={()=>setScreen('botones')}/>
                     </View>
                 )
-    }  
+    }
+
+
   }
+
 
 const styles = StyleSheet.create({})
