a090e68298253417c540aaacef62ea5c944002ea
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-15 19:19:40 -0600

    Utilizamos nrcs para que nos de un esqueleto y lo modificamos

---
+++ intro/screens/MenuScreen.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+
+export default function MenuScreen() {
+    return (
+      <View>
+        <Text>MenuScreen</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
