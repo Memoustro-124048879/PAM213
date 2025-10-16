42b6160066c64f78fb894150021170fa4570e8a9
Memoustro-124048879 <124048879@upq.edu.mx>
2025-10-15 19:21:21 -0600

    Importamos contadorScreen y BotonesScreen

--- intro/screens/MenuScreen.js
+++ intro/screens/MenuScreen.js
@@ -1,5 +1,7 @@
 import { Text, StyleSheet, View } from 'react-native'
 import React, { useState } from 'react'
+import contadorScreen from './contadorScreen'
+import BotonesSceen from './BotonesSceen'
 
 export default function MenuScreen() {
     return (
