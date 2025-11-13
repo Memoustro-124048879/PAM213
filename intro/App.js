9f3dbdfdc36acfdf4084a7a73a61a8a17f553fe0
Memoustro-124048879 <124048879@upq.edu.mx>
2025-11-13 16:07:31 -0600

    Bottom Sheet Completada

--- intro/App.js
+++ intro/App.js
@@ -1,9 +1,13 @@
-
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import MenuScreen from "./screens/MenuScreen";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
 
   return (
-    <MenuScreen></MenuScreen>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <MenuScreen></MenuScreen>
+    </GestureHandlerRootView>
   );
 }
