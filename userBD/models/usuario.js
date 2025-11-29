99f48e5eca7dc343129b05b13c58525a2bb0e82e
Memoustro-124048879 <124048879@upq.edu.mx>
2025-11-28 22:30:53 -0600

    Practica No.20: Update y Delete con SQLite local

--- userBD/models/usuario.js
+++ userBD/models/usuario.js
@@ -1,17 +1,17 @@
 export class Usuario{
     constructor(id,nombre,fechaCreacion){
-        this.id=id;
-        this.nombre=nombre;
+        this.id = id;
+        this.nombre = nombre;
         this.fechaCreacion=fechaCreacion || new Date().toISOString();
     }
 
-    //validaciones del modelo
-    static validar(nombre){
-        if(!nombre || nombre.trim().length===0){
-            throw new Error('El nombre no puede estar vacio');
+    //Validaciones del modelo
+    static validar (nombre){
+        if (!nombre || nombre.trim().length === 0){
+            throw new Error('El nombre no puede estar vacío');
         }
-        if(nombre.length>50){
-            throw new Error('El nombre no puede tener mas de 50 caracteres');
+        if (nombre.length > 50){
+            throw new Error('El nombre no puede tener más de 50 caracteres');
         }
         return true;
     }
